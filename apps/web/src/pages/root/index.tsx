56b815e49e5d3f1326ca07c4c2fb8b3b5ca77f49
kmilo8346 <kmilo8346@gmail.com>
2024-08-06 16:46:15 -0400

    update(root): changed the title

--- apps/web/src/pages/root/index.tsx
+++ apps/web/src/pages/root/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.module.scss';
 export function RootPage() {
   return (
     <div className={styles.root}>
-      <h1>Courses</h1>
+      <h1>Courses List</h1>
     </div>
   );
 }
