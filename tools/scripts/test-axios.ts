385bd8a914b5efa711f1f88c17a5786ccb9f9e93
kmilo8346 <kmilo8346@gmail.com>
2024-08-21 10:37:53 -0400

    feature(scripts): added scripts

---
+++ tools/scripts/test-axios.ts
@@ -0,0 +1,11 @@
+const run = async () => {
+  console.log('Test axios arian');
+
+  // Juega con axios
+  // Probar el login (public)
+  // Crear el usuario (public)
+  // Get all usuarios (private)
+  // Eliminar usuario (private)
+};
+
+run();
